78e665281f1a53ecb80fb114a422c02be0c82d64
Florent gravin <florent.gravin@camptocamp.com>
2018-02-07 15:18:45 +0100

    Create repo with some examples

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vector tiles support in openlayers, test & metrics
+Provide different examples to compare vector tiles rendering performance.
+## Install
+```
+npm install
+```
+## Run
+```
+npm start
+```
+## Open
+
+localhost:3000/simple.html
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const Dashboard = require('webpack-dashboard/plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ls = require('ls');
+
+const plugins = [
+  new Dashboard({
+    color: 'cyan'
+  }),
+  new webpack.EnvironmentPlugin(
+    Object.assign({
+      NODE_ENV: 'development'
+    })
+  )
+];
+const entry = {};
+
+for (const filename of ls('examples/*.html')) {
+  const name = filename.name;
+  entry[name] = [`./examples/${name}.js`];
+  plugins.push(
+    new HtmlWebpackPlugin({
+      template: `examples/${name}.html`,
+      chunksSortMode: 'manual',
+      filename: name + '.html',
+      chunks: ['common', name],
+    }),
+  );
+}
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+  name: 'common',
+  chunks: Object.keys(entry),
+}));
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  entry: entry,
+  plugins: plugins,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ]
+      }
+    ]
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    disableHostCheck: true,
+    overlay: true
+  }
+};

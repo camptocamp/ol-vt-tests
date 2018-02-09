9c1a9e039a7f1928672b468dc1752c2f4eb710e9
Frederic Junod <frederic.junod@camptocamp.com>
2018-02-09 08:48:11 +0100

    Use OpenLayers 5.0.0beta.3

--- examples/simple.js
+++ examples/simple.js
@@ -1,8 +1,7 @@
-import 'ol/ol.css';
-import Map from 'ol/map';
-import View from 'ol/view';
-import TileLayer from 'ol/layer/tile';
-import XYZ from 'ol/source/xyz';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
 
 new Map({
   target: 'map',

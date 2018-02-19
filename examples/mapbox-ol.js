54894b6e3e83acb2f491670ec82486c6cf03f2a1
Roman Zoller <roman.zoller@camptocamp.com>
2018-02-19 15:10:59 +0100

    Add Mapbox GL JS integration examples

---
+++ examples/mapbox-ol.js
@@ -0,0 +1,60 @@
+import mapboxgl from 'mapbox-gl';
+
+import OLLayerTile from 'openlayers/layer/Tile.js';
+import OLMap from 'openlayers/Map.js';
+import OLTileJSON from 'openlayers/source/TileJSON.js';
+import OLView from 'openlayers/View.js';
+import { get as getTransform } from 'openlayers/proj/transforms.js';
+
+mapboxgl.accessToken = "pk.eyJ1Ijoicm9tYW56b2xsZXIiLCJhIjoiY2pkand5dzJkMG1jdTMzbzJqMmZ5cDUycSJ9.djXLa0ZfFbls1yUu35I80Q";
+
+const map = new mapboxgl.Map({
+    container: "map", // container id
+    style: "mapbox://styles/mapbox/streets-v9", // stylesheet location
+    center: [6.5124174, 46.5284586], // starting position
+    maxZoom: 17,
+    zoom: 10 // starting zoom
+});
+
+const swisstopoBauzonenSource = new OLTileJSON({
+    tileJSON: {
+        "attribution": "&copy; swisstopo",
+        "bounds": [
+            5.140242,
+            45.398181,
+            11.47757,
+            48.230651
+        ],
+        "id": "bauzonen",
+        "maxzoom": 18,
+        "minzoom": 0,
+        "type": "raster",
+        "tileSize": 256,
+        "tiles": [
+            "https://wmts.geo.admin.ch/1.0.0/ch.are.bauzonen/default/current/3857/{z}/{x}/{y}.png"
+        ],
+    }
+});
+
+const transform = getTransform("EPSG:4326", "EPSG:3857");
+
+const olMap = new OLMap({
+    layers: [
+        new OLLayerTile({
+            opacity: 0.3,
+            source: swisstopoBauzonenSource
+        })
+    ],
+    target: "olmap",
+    view: new OLView({
+        center: transform([6.5124174, 46.5284586]),
+        maxZoom: 18,
+        zoom: 11
+    })
+});
+
+map.on("move", () => {
+    const olMapView = olMap.getView();
+    olMapView.setCenter(transform(map.getCenter().toArray()));
+    olMapView.setZoom(map.getZoom() + 1);
+});
---
+++ examples/mapbox-standalone.js
@@ -0,0 +1,41 @@
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = "pk.eyJ1Ijoicm9tYW56b2xsZXIiLCJhIjoiY2pkand5dzJkMG1jdTMzbzJqMmZ5cDUycSJ9.djXLa0ZfFbls1yUu35I80Q";
+
+const map = new mapboxgl.Map({
+    container: "map", // container id
+    style: "mapbox://styles/mapbox/streets-v9", // stylesheet location
+    center: [6.5124174, 46.5284586], // starting position
+    maxZoom: 17,
+    zoom: 10 // starting zoom
+});
+
+map.on("load", () => {
+    map.addSource("bauzonen", {
+        "attribution": "&copy; swisstopo",
+        "bounds": [
+            5.140242,
+            45.398181,
+            11.47757,
+            48.230651
+        ],
+        "maxzoom": 18,
+        "minzoom": 0,
+        "type": "raster",
+        "tileSize": 256,
+        "tiles": [
+            "https://wmts.geo.admin.ch/1.0.0/ch.are.bauzonen/default/current/3857/{z}/{x}/{y}.png"
+        ],
+    });
+
+    map.addLayer({
+        "id": "bauzonen",
+        "type": "raster",
+        "source": "bauzonen",
+        "minzoom": 0,
+        "maxzoom": 18,
+        "paint": {
+            "raster-opacity": 0.3,
+        },
+    });
+});

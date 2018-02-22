a6706f0e96806ce720278f177e3ded547ae3cbc7
Florent gravin <florent.gravin@camptocamp.com>
2018-02-22 14:48:08 +0100

    Add interactions with mapbox layer

--- examples/ol-mapbox-layer.js
+++ examples/ol-mapbox-layer.js
@@ -2,6 +2,7 @@ import Map from "openlayers/Map.js";
 import View from "openlayers/View.js";
 import TileLayer from "openlayers/layer/Tile.js";
 import MapBox from "openlayers/layer/MapBox";
+import Overlay from "openlayers/Overlay";
 import OLTileJSON from "openlayers/source/TileJSON.js";
 import {get as getTransform} from "openlayers/proj/transforms.js";
 
@@ -78,6 +79,81 @@ const trailLayer = new TileLayer({
 
 map.addLayer(trailLayer);
 
+const container = document.getElementById('popup');
+const content = document.getElementById('popup-content');
+const closer = document.getElementById('popup-closer');
+
+const overlay = new Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250
+  }
+});
+closer.onclick = function() {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+map.getOverlays().push(overlay);
+
+const mbmap = mbLayer.getMbMap();
+let parkColor;
+
+mbmap.on("load", () => {
+  parkColor = mbmap.getPaintProperty('NationalPark', 'fill-color');
+  mbmap.addLayer({
+    "id": "river-hover",
+    "type": "fill",
+    "source": "swissbasemap",
+    "source-layer": "swissbasemap-layer",
+    "layout": {},
+    "paint": {
+      "fill-color": "blue",
+      "fill-opacity": 1
+    },
+    "filter": ["==", "UUID", ""]
+  });
+
+});
+
+mbmap.on('mouseenter', 'NationalPark', (e) => {
+  mbmap.setPaintProperty('NationalPark', 'fill-color', 'red');
+});
+mbmap.on('mouseleave', 'NationalPark', (e) => {
+  mbmap.setPaintProperty('NationalPark', 'fill-color', parkColor);
+});
+
+mbmap.on("mousemove", "GEWAESSER Fliessgewaesser PLY", function(e) {
+  setMousePointer();
+  mbmap.setFilter("river-hover", ["==", "UUID", e.features[0].properties.UUID]);
+});
+
+// Reset the state-fills-hover layer's filter when the mouse leaves the layer.
+mbmap.on("mouseleave", "GEWAESSER Fliessgewaesser PLY", function() {
+  removeMousePointer();
+  mbmap.setFilter("river-hover", ["==", "UUID", ""]);
+});
+
+const displayPopup =  function (e) {
+  const description = e.features[0].properties.layerid;
+  content.innerHTML = `<p>${description}</p>`;
+  overlay.setPosition(transformFromLatLng([e.lngLat.lng, e.lngLat.lat]));
+};
+
+const setMousePointer = function () {
+  map.getTargetElement().style.cursor = 'pointer';
+};
+const removeMousePointer = function () {
+  map.getTargetElement().style.cursor = '';
+};
+
+['OEV Einspur', 'OEV Mehrspur'].forEach((layer) => {
+  mbmap.on('click', layer, displayPopup);
+  mbmap.on('mouseenter', layer, setMousePointer);
+  mbmap.on('mouseleave', layer, removeMousePointer);
+});
 
 const mbToggleButton = document.getElementById('mbToggle');
 const mbOpacityInput = document.getElementById('mbOpacity');
